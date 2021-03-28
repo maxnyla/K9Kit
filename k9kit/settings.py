a3a05690e3e215fe7f3b8f173009da46b96c628b
maxnyla <sariflor@yahoo.co.uk>
2021-03-28 19:54:05 +0000

    add home app and templates

--- k9kit/settings.py
+++ k9kit/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
