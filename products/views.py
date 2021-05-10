0146bd75d597532c33909c13d7316c9e262a8373
maxnyla <sariflor@yahoo.co.uk>
2021-05-10 23:45:37 +0000

    fixing paddings and margins for index.html page

--- products/views.py
+++ products/views.py
@@ -22,7 +22,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -35,10 +36,12 @@ def all_products(request):
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
+    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't specify any search criteria")
+                messages.error(
+                    request, "You didn't specify any search criteria")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
