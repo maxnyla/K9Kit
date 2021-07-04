3f7e4a62dce4a90a3875ec616143ba007b484924
maxnyla <sariflor@yahoo.co.uk>
2021-07-04 16:34:37 +0000

    adding edit and delete product functionality and urls

--- products/views.py
+++ products/views.py
@@ -76,9 +76,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product was successfully added')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please check the form is valid.')
     else:
@@ -92,7 +92,7 @@ def add_product(request):
     return render(request, template, context)
 
 def edit_product(request, product_id):
-    """ Edit a product in the store """
+    """ Edit an existing product """
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -113,3 +113,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete an existing product """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
