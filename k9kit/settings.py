cf6f9db03443990d678f612f13e7123e30aade04
maxnyla <sariflor@yahoo.co.uk>
2021-07-07 18:03:04 +0000

    starting deployment to Heroku

--- k9kit/settings.py
+++ k9kit/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '=_p2mhthmk!os#%v!nwpkm2(5ga$ukbwo3uhx%re-d5sh)qpdj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['k9kit.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'k9kit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
