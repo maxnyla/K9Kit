99ba9d01e3c990a4acac6c039330fe929bd5e5ff
maxnyla <sariflor@yahoo.co.uk>
2021-08-24 20:55:20 +0000

    adding tips template

--- k9kit/urls.py
+++ k9kit/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('reviews/', include('reviews.urls')),
     path('newsletters/', include('newsletters.urls')),
+    path('tips/', include('tips.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- tips/views.py
+++ tips/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Tip
+#from .forms import NewsletterForm
+
+
+def tips(request):
+    """ A view showing all site tips """
+    tips = Tip.objects.all()
+
+    context = {
+        'tips': tips,
+    }
+
+    return render(request, 'tips/tips.html', context)
